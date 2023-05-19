50d3bd07896d54ff35e019925e3019bf0b9954f9
Albit0 <agustin_010@live.com>
2023-05-19 00:03:03 -0300

    Primer commit con algunas rutas de user creadas y jwt implementado, cambios

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,36 +2,57 @@ const mongoose = require("mongoose"); // Erase if already required
 const bcrypt = require("bcrypt");
 
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-    index: true,
+var userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: [
+      {
+        type: ObjectId,
+        ref: "Address",
+      },
+    ],
+    whislist: [
+      {
+        type: ObjectId,
+        ref: "Product",
+      },
+    ],
   },
-  lastname: {
-    type: String,
-    required: true,
-    index: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: "user",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
   const salt = bcrypt.genSaltSync(10);
