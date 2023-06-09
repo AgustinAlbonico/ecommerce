8a933b4f4267037a345426bc835156066f608106
Albit0 <agustin_010@live.com>
2023-06-09 10:20:51 -0300

    Termine endpoints de prodcat

---
+++ server/controller/categoryController.js
@@ -0,0 +1,69 @@
+const Category = require('../models/prodcategoryModel');
+const asyncHandler = require('express-async-handler');
+const { validateMongodbID } = require('../utils/validateMongodbID');
+
+//Crear categoria
+const createCategory = asyncHandler(async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.json(category);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//Update category
+const updateCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbID(id);
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//Delete category
+const deleteCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbID(id);
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(id);
+    res.json(deletedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//Get a category
+const getCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbID(id);
+  try {
+    const category = await Category.findById(id);
+    res.json(category);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//Get categories
+const getCategories = asyncHandler(async (req, res) => {
+  try {
+    const category = await Category.find();
+    res.json(category);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategory,
+  getCategories,
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,13 @@ const authRoute = require('./authRoute');
 const userRoute = require('./userRoute');
 const productRoute = require('./productRoute');
 const blogRoute = require('./blogRoute');
+const categoryRoute = require('./categoryRoute');
 
 //Rutas
 Router.use('/auth', authRoute);
 Router.use('/users', userRoute);
 Router.use('/products', productRoute);
 Router.use('/blogs', blogRoute);
+Router.use('/category', categoryRoute);
 
 module.exports = Router;
