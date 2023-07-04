b82857869dab25b2451250db3a354207ed8efaa9
Albit0 <agustin_010@live.com>
2023-07-04 20:20:52 -0300

    Header

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
